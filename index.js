8c1ba55c94d4ed71937ef236b6edb9ce7fc1108e
Ivan <ivan93mail@gmail.com>
2019-12-06 16:35:13 +0200

    second commit

--- index.js
+++ index.js
@@ -1,88 +1,496 @@
-let questions = [
+const questions = [
+  {
+     "archetypeId": 1,
+     "question": "Я исхожу из того, что люди, с которыми я общаюсь, заслуживают доверия"
+   },
+   {
+     "archetypeId": 2,
+     "question": "Я не люблю быть в одиночестве"
+   },
+   {
+     "archetypeId": 3,
+     "question": "Соревнования заводят и активизируют меня"
+   },
     {
-       "archetypeId": 1,
-       "question": "Я исхожу из того, что люди, с которыми я общаюсь, заслуживают доверия"
-     },
-     {
-       "archetypeId": 2,
-       "question": "Я не люблю быть в одиночестве"
-     },
-     {
-       "archetypeId": 3,
-       "question": "Соревнования заводят и активизируют меня"
-     }
-   ];
-   
-   let answers = [
-     {
-       'weight': 1,
-       'answer': "Полностью не согласен"
-     },
-     {
-       'weight': 2,
-       'answer': "Не согласен"
-     },
-     {
-       'weight': 3,
-       'answer': "Не уверен"
-     },
-     {
-       'weight': 4,
-       'answer': "Согласен"
-     },
-     {
-       'weight': 5,
-       'answer': "Полностью согласен"
-     }
-   ];
-   
-   let form = document.createElement('form');
-   form.className = 'wrapper';
-   document.body.append(form);
-   
-   function renderForm(x,y) {
-     
-     let fieldset = document.createElement('fieldset');
-     form.append(fieldset);
-     fieldset.className = 'container';
-   
-     let legend = document.createElement('legend');
-     fieldset.append(legend);
-     legend.className = 'question';
-     legend.textContent = x.question;
-     
-     answers.forEach((i,v) => {
-       renderInputAndLabel(i,v);
-     });
-     
-     function renderInputAndLabel(i,v) {
-       let label = document.createElement('label');
-       fieldset.append(label);
-       label.className = 'label';
-       label.textContent = answers[v].answer;
-       
-       let input = document.createElement('input');
-       label.prepend(input);
-       input.className = 'label';
-       input.setAttribute('type', 'radio');
-     }
+     "archetypeId": 1,
+     "question": "Я верю, что люди, в действительности, не хотят делать друг другу больно"
+   },
+   {
+     "archetypeId": 2,
+     "question": "Я знаю, что важно не ожидать слишком многого"
+   },
+   {
+     "archetypeId": 3,
+     "question": "Я готов рисковать собой, чтобы защищать свои принципы и права"
+   },
+   {
+     "archetypeId": 1,
+     "question": "Вокруг меня есть люди, на которых можно положиться, они смогут позаботятся обо мне"
+   },
+   // {
+   //   "archetypeId": 2,
+   //   "question": "Дружба - это самое важное в жизни"
+   // },
+   // {
+   //   "archetypeId": 3,
+   //   "question": "В большинстве случаев я справляюсь со страхом и делаю то, что необходимо сделать"
+   // },
+   // {
+   //   "archetypeId": 1,
+   //   "question": "Я чувствую себя в безопасности"
+   // },
+   // {
+   //   "archetypeId": 2,
+   //   "question": "Важна не победа, а участие"
+   // },
+   // {
+   //   "archetypeId": 3,
+   //   "question": "Я всегда смогу противостоять обидчикам"
+   // },
+   // {
+   //   "archetypeId": 1,
+   //   "question": "Я знаю, что мои потребности будут удовлетворены, в итоге, я получу, что хочу"
+   // },
+   // {
+   //   "archetypeId": 2,
+   //   "question": "Меня раздражают хвастуны и выскочки"
+   // },
+   // {
+   //   "archetypeId": 3,
+   //   "question": "Меня устраивает, что я напористый и решительный"
+   // },
+   // {
+   //   "archetypeId": 1,
+   //   "question": "Мир – это безопасное место"
+   // },
+   // {
+   //   "archetypeId": 2,
+   //   "question": "Я не люблю выделяться из толпы"
+   // },
+   // {
+   //   "archetypeId": 3,
+   //   "question": "В условиях соревнования я проявляю все свои лучшие качества"
+   // },
+   // {
+   //   "archetypeId": 4,
+   //   "question": "Я нахожу смысл, полноту жизни в том, что помогаю другим людям"
+   // },
+   // {
+   //   "archetypeId": 5,
+   //   "question": "Я всегда ищу более комфортные условия"
+   // },
+   // {
+   //   "archetypeId": 6,
+   //   "question": "Я, в принципе, люблю всех людей"
+   // },
+   // {
+   //   "archetypeId": 4,
+   //   "question": "Мне кажется, что легче делать что-то для других, чем для себя"
+   // },
+   // {
+   //   "archetypeId": 5,
+   //   "question": "Я всегда ищу способы усовершенствовать себя"
+   // },
+   // {
+   //   "archetypeId": 6,
+   //   "question": "Я нахожу смысл жизни в отношениях"
+   // },
+   // {
+   //   "archetypeId": 4,
+   //   "question": "Мне нравится заботиться о других людях"
+   // },
+   // {
+   //   "archetypeId": 5,
+   //   "question": "Я чувствую себя тревожно"
+   // },
+   // {
+   //   "archetypeId": 6,
+   //   "question": "Мне нравится помогать людям встречаться и знакомиться друг с другом"
+   // },
+   // {
+   //   "archetypeId": 4,
+   //   "question": "Мне тяжело говорить нет"
+   // },
+   // {
+   //   "archetypeId": 5,
+   //   "question": "Я стараюсь до конца, чтобы получить что-то лучшее"
+   // },
+   // {
+   //   "archetypeId": 6,
+   //   "question": "Близкие, искренние отношения это самое главное для меня"
+   // },
+   // {
+   //   "archetypeId": 4,
+   //   "question": "Интересы других важнее моих собственных интересов"
+   // },
+   // {
+   //   "archetypeId": 5,
+   //   "question": "Я все время нахожусь в поисках новых возможностей"
+   // },
+   // {
+   //   "archetypeId": 6,
+   //   "question": "Я - романтик и мечтатель"
+   // },
+   // {
+   //   "archetypeId": 4,
+   //   "question": "Доброта для меня наивысшая ценность"
+   // },
+   // {
+   //   "archetypeId": 5,
+   //   "question": "Для меня очень важно сохранять личную свободу"
+   // },
+   // {
+   //   "archetypeId": 6,
+   //   "question": "Я чувствую себя более наполненным, когда я влюблен"
+   // },
+   // {
+   //   "archetypeId": 7,
+   //   "question": "Я всегда найду выход из неприятной ситуации"
+   // },
+   // {
+   //   "archetypeId": 8,
+   //   "question": "Я сам творец своей собственной жизни"
+   // },
+   // {
+   //   "archetypeId": 9,
+   //   "question": "У меня хорошо получается подбирать задачи, которые соответствуют способностям людей"
+   // },
+   // {
+   //   "archetypeId": 7,
+   //   "question": "Моя жизнь быстро меняется"
+   // },
+   // {
+   //   "archetypeId": 8,
+   //   "question": "У меня намного больше прекрасных идей, чем времени на их выполнение"
+   // },
+   // {
+   //   "archetypeId": 9,
+   //   "question": "У меня есть лидерские качества"
+   // },
+   // {
+   //   "archetypeId": 7,
+   //   "question": "В моей жизни присутствует какая-то пустота"
+   // },
+   // {
+   //   "archetypeId": 8,
+   //   "question": "Часто успех и большие достижения мне даются очень легко"
+   // },
+   // {
+   //   "archetypeId": 9,
+   //   "question": "Я люблю, чтобы все было под контролем"
+   // },
+   // {
+   //   "archetypeId": 7,
+   //   "question": "Правила существуют для того, чтобы их нарушать"
+   // },
+   // {
+   //   "archetypeId": 8,
+   //   "question": "Вдохновение легко приходит ко мне"
+   // },
+   // {
+   //   "archetypeId": 9,
+   //   "question": "Мне нравится брать на себя ответственность за что-либо"
+   // },
+   // {
+   //   "archetypeId": 7,
+   //   "question": "Недавний опыт заставил меня переосмыслить кто я есть"
+   // },
+   // {
+   //   "archetypeId": 8,
+   //   "question": "Люди считают меня творческим человеком"
+   // },
+   // {
+   //   "archetypeId": 9,
+   //   "question": "Я часто беру на себя лидерство в кризисных ситуациях, когда вещи уходят из под контроля"
+   // },
+   // {
+   //   "archetypeId": 7,
+   //   "question": "Я расстраиваюсь из-за неудач и проигрышей"
+   // },
+   // {
+   //   "archetypeId": 8,
+   //   "question": "У меня хорошее воображение"
+   // },
+   // {
+   //   "archetypeId": 9,
+   //   "question": "Мой долг – выполнять взятые на себя обязательства"
+   // },
+   // {
+   //   "archetypeId": 10,
+   //   "question": "Изменение моих мыслей приводит к изменениям в моей жизни"
+   // },
+   // {
+   //   "archetypeId": 11,
+   //   "question": "Я верю, что существует много позитивных способов смотреть на одну и ту же вещь/вопрос"
+   // },
+   // {
+   //   "archetypeId": 12,
+   //   "question": "Я люблю шутить и смешить людей"
+   // },
+   // {
+   //   "archetypeId": 10,
+   //   "question": "Я верю, что все и всё в этом мире взаимосвязаны"
+   // },
+   // {
+   //   "archetypeId": 11,
+   //   "question": "Я стремлюсь к объективности"
+   // },
+   // {
+   //   "archetypeId": 12,
+   //   "question": "Я подхожу с юмором к людям, которые слишком серьезные"
+   // },
+   // {
+   //   "archetypeId": 10,
+   //   "question": "Я люблю преобразовывать/ трансформировать ситуации"
+   // },
+   // {
+   //   "archetypeId": 11,
+   //   "question": "Меня часто посещают глубокие мысли"
+   // },
+   // {
+   //   "archetypeId": 12,
+   //   "question": "Меня считают прикольным, веселым"
+   // },
+   // {
+   //   "archetypeId": 10,
+   //   "question": "Часто мое присутствие - катализатор изменений вокруг"
+   // },
+   // {
+   //   "archetypeId": 11,
+   //   "question": "Я пытаюсь найти истину, которая скрывается за иллюзиями"
+   // },
+   // {
+   //   "archetypeId": 12,
+   //   "question": "Я посмеиваюсь над абсурдностью нашей жизни"
+   // },
+   // {
+   //   "archetypeId": 10,
+   //   "question": "Моя эффективность зависит от состояния моей души"
+   // },
+   // {
+   //   "archetypeId": 11,
+   //   "question": "Люди ждут от меня советов"
+   // },
+   // {
+   //   "archetypeId": 12,
+   //   "question": "Я предпочитаю жить сегодняшним днем"
+   // },
+   // {
+   //   "archetypeId": 10,
+   //   "question": "Процесс моего собственного умиротворения помогает мне помочь другим"
+   // },
+   // {
+   //   "archetypeId": 11,
+   //   "question": "Я люблю вызовы, сложные задачи, которые заставляют меня думать"
+   // },
+   // {
+   //   "archetypeId": 12,
+   //   "question": "Нет ничего лучше, чем посмеяться от души"
+   // }
+ ];
+ const answers = [
+   {
+     'weight': 1,
+     'answer': "Полностью не согласен"
+   },
+   {
+     'weight': 2,
+     'answer': "Не согласен"
+   },
+   {
+     'weight': 3,
+     'answer': "Не уверен"
+   },
+   {
+     'weight': 4,
+     'answer': "Согласен"
+   },
+   {
+     'weight': 5,
+     'answer': "Полностью согласен"
    }
+ ];
+ const archetypes = [
+   {
+       counting: [],
+       "id": 1,
+       "name": "Невинный",
+       "shortDescription": "Полезный, благотворный, чистый, честный, всепрощающий, достойный доверия, счастливый, оптимистичный, любит простые удовольствия"
+   },
+   {
+       counting: [],
+       "id": 2,
+       "name": "Славный малый",
+       "shortDescription": "Неброский, прямолинейный, как все, надежный, практичный, приземленный, предсказуемый, статус кво, традиционный"
+   },
+   {
+       counting: [],
+       "id": 3,
+       "name": "Герой",
+       "shortDescription": "Воитель, соревнующийся, агрессивный, победитель, принципиальный, идеалист, отрицает все плохое, стремится улучшить мир, гордый, смелый, отважный"
+   },
+   {
+   counting: [],
+     "id": 4,
+     "name": "Родитель",
+     "shortDescription": "Альтруистический, не думающий о себе, заботливый, сострадательный, сопереживающий, готовы помочь, щедрый, благородный"
+   },
+   {
+   counting: [],
+     "id": 5,
+     "name": "Искатель",
+     "shortDescription": "Исследователь, рискованный, опасный, независимый, самоустремленный, самореализующийся, ценящий свободу"
+   },
+   {
+   counting: [],
+     "id": 6,
+     "name": "Любовник",
+     "shortDescription": "В поисках настоящей любви, интимный, чувствительный, страстный, сексуальный, соблазнительный, ищет наслаждения, баловень, живет эмоциями"
+   },
+   {
+   counting: [],
+     "id": 7,
+     "name": "Бунтарь",
+     "shortDescription": "Мятежный, шокирующий, пробивной, разрушительный, cильный, влиятельный, революционный, освободительны"
+   },
+   {
+   counting: [],
+     "id": 8,
+     "name": "Творец",
+     "shortDescription": "Новатор, фантазер, артистичный, экспериментатор, идет на риск, амбициозный"
+   },
+   {
+   counting: [],
+     "id": 9,
+     "name": "Правитель",
+     "shortDescription": "Управленец, организатор, Квалифицированный, действительный, эффективный, продуктивный, уверенный, ответственный, ролевая модель"
+   },
+   {
+   counting: [],
+     "id": 10,
+     "name": "Волшебник",
+     "shortDescription": "Шаман, целитель, божественный, интуитивный, волшебные моменты и ритуалы, катализатор для изменений, харизма и обаяние"
+   },
+   {
+   counting: [],
+     "id": 11,
+     "name": "Мудрец",
+     "shortDescription": "Мыслитель, философ, задумчивый, эксперт, советник, учитель, самоуверенный, самоконтролируемый, независимый"
+   },
+   {
+   counting: [],
+     "id": 12,
+     "name": "Шут",
+     "shortDescription": "Шутник, клоун, фокусник, игривый, веселый, принимает вещи легко, создает веселье/хаос, импульсивный, спонтанный, живет одним моментом."
+   }
+ ];
+ 
+ let form = document.createElement('form');
+ form.className = 'wrapper';
+ document.body.append(form);
+ form.setAttribute('method', 'post');
+ // form.setAttribute('onsubmit', 'FA();return false;');
+ form.setAttribute('name', 'set_from');
+ 
+ let attributeDataId;
+ 
+ function renderForm(questions) {
    
-   questions.forEach((x) => {
-      renderForm(x);  
+   let fieldset = document.createElement('fieldset');
+   form.append(fieldset);
+   fieldset.className = 'container';
+ 
+   let legend = document.createElement('legend');
+   fieldset.append(legend);
+   legend.className = 'question';
+   legend.textContent = questions.question;
+   fieldset.setAttribute('data-id', questions.archetypeId);
+   attributeDataId = fieldset.getAttribute('data-id');
+   
+   function renderInputAndLabel(answerItem,answerIndex) {
+     let label = document.createElement('label');
+     fieldset.append(label);
+     label.className = 'label';
+     label.textContent = answers[answerIndex].answer;
+     
+     let input = document.createElement('input');
+     label.prepend(input);
+     input.className = 'input';
+     input.setAttribute('type', 'radio');
+     input.setAttribute('value', answerIndex + 1);
+     input.setAttribute('data-id', attributeDataId);
+   }
+   answers.forEach((answerItem,answerIndex) => {
+     renderInputAndLabel(answerItem,answerIndex);
    });
+ }
+ 
+ questions.forEach((question,qwsIndex) => {
+   renderForm(question,qwsIndex);  
+ });
+ 
+ let questionsCollection = document.querySelectorAll('.container');
+ questionsCollection.forEach((collectionItem,collectionIndex) => { //name соответсвует порядку вопроса
    
-   let containerCollection = document.querySelectorAll('.container');
-   //set attribute 'name' for inputs
-   containerCollection.forEach((j,v) => {
-     j.children[1].children[0].setAttribute('name', v);
-     j.children[2].children[0].setAttribute('name', v);
-     j.children[3].children[0].setAttribute('name', v);
-     j.children[4].children[0].setAttribute('name', v);
-     j.children[5].children[0].setAttribute('name', v);
-   });
-   
-   let submit = document.createElement('input');
-   submit.setAttribute('type', 'submit');
-   submit.className = 'btn-submit';
-   form.append(submit);
+   collectionItem.children[1].children[0].setAttribute('name', collectionIndex);
+   collectionItem.children[2].children[0].setAttribute('name', collectionIndex);
+   collectionItem.children[3].children[0].setAttribute('name', collectionIndex);
+   collectionItem.children[4].children[0].setAttribute('name', collectionIndex);
+   collectionItem.children[5].children[0].setAttribute('name', collectionIndex);
+ });
+ 
+ let submit = document.createElement('input');
+ submit.setAttribute('type', 'submit');
+ submit.setAttribute('onclick', 'FA(sortByCount); return false;');
+ // submit.addEventListener('click', FA);
+ submit.className = 'btn-submit';
+ form.append(submit);
+ 
+ function FA(callback) {
+   let radio = document.getElementsByClassName('input');
+   //наполняет массив каждого архетипа ответами из формы
+     for (let radioItem = 0; radioItem < radio.length; radioItem++) {
+       for (let archetype = 0; archetype < archetypes.length; archetype++) {
+          if (radio[radioItem].checked && 
+              radio[radioItem].getAttribute('data-id') == archetypes[archetype].id) {
+            archetypes[archetype].counting.push(radio[radioItem].value);
+          }
+       }
+     }
+    archetypes[0].counting = archetypes[0].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[1].counting = archetypes[1].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[2].counting = archetypes[2].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[3].counting = archetypes[3].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[4].counting = archetypes[4].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[5].counting = archetypes[5].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[6].counting = archetypes[6].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[7].counting = archetypes[7].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[8].counting = archetypes[8].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[9].counting = archetypes[9].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[10].counting = archetypes[10].counting.reduce((sum, current) => sum + parseInt(current), 0);
+    archetypes[11].counting = archetypes[11].counting.reduce((sum, current) => sum + parseInt(current), 0);
+ 
+   callback(archetypes);
+   form.remove();
+   display.innerHTML = `
+     ${archetypes[1].name} <br/> ${archetypes[1].shortDescription} <hr/>
+     ${archetypes[2].name} <br/> ${archetypes[2].shortDescription} <hr/>
+     ${archetypes[3].name} <br/> ${archetypes[3].shortDescription} <hr/>
+     ${archetypes[4].name} <br/> ${archetypes[4].shortDescription} <hr/>
+     ${archetypes[5].name} <br/> ${archetypes[5].shortDescription} <hr/>
+     ${archetypes[6].name} <br/> ${archetypes[6].shortDescription} <hr/>
+     ${archetypes[7].name} <br/> ${archetypes[7].shortDescription} <hr/>
+     ${archetypes[8].name} <br/> ${archetypes[8].shortDescription} <hr/>
+     ${archetypes[9].name} <br/> ${archetypes[9].shortDescription} <hr/>
+     ${archetypes[10].name} <br/> ${archetypes[10].shortDescription} <hr/>
+     ${archetypes[11].name} <br/> ${archetypes[11].shortDescription} <hr/>`;
+ }
+ 
+ function sortByCount(arr) {
+   arr.sort((a, b) => a.counting > b.counting ? -1 : 1);
+ }
+ 
+ let display = document.createElement('div');
+ display.className = "display";
+ document.body.append(display);
+ 
